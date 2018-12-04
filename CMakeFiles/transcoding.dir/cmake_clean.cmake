3002d5f57a52f0036e08b50aa1e68f1b99a323fe
Chris6002 <qsun15@u.rochester.edu>
2018-12-04 14:28:43 -0500

    first

---
+++ CMakeFiles/transcoding.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/transcoding.dir/transcoding.cpp.o"
+  "transcoding.pdb"
+  "transcoding"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/transcoding.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
